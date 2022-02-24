3aa68fa7f682ebefe73941c587af74a15a7061f0
lex.ari <amarianocello@gmail.com>
2022-02-24 14:49:40 -0800

    Added Basic Methods to JUnitTest1.java - Alex

--- src/JUnitTest1.java
+++ src/JUnitTest1.java
@@ -1,2 +1,22 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class JUnitTest1 {
+    @Before
+    public static void setUp() throws Exception{
+
+    }
+
+    @Test
+    public void testAdd(){
+
+    }
+
+    @After
+    public static void tearDown() throws Exception{
+
+    }
+
 }
