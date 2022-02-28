fb9d3c492b7970a41cfaa42183421deb55e57ec0
Iker Goni <igoni@cpp.edu>
2022-02-28 15:33:46 -0800

    Fixed intersection for resizableArrayBag

--- src/ResizableArrayBag.java
+++ src/ResizableArrayBag.java
@@ -225,7 +225,7 @@ public class ResizableArrayBag<T> implements BagInterface<T>{
         T[] array1 = this.toArray();
         T[] array2 = aBag.toArray(); 
         @SuppressWarnings("unchecked")
-        ResizableArrayBag<T> intersectionBag = new ResizableArrayBag();
+        ResizableArrayBag<T> intersectionBag = new ResizableArrayBag(this.getCurrentSize());
         for(int i = 0; i < array1.length; i++){
             for(int j = 0; j < array2.length; j++){
                 if(array1[i] != null && array2[j] != null && array1[i].equals(array2[j])){
