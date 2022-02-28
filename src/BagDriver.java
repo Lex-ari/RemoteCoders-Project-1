8d5b48f3bd7ccdae0bf3fc67fadbc7aadbcad98f
Koki <kyamaguchi@cpp.edu>
2022-02-28 13:58:05 -0800

    Creating add method in the BagDriver class also fix to TestRunner.testLinked bag and resisable array

--- src/BagDriver.java
+++ src/BagDriver.java
@@ -1,7 +1,21 @@
 
 public class BagDriver{
     public static void main(String[] args){
-        TestSuite.testResizableArrayBag();
-        TestSuite.testLinkedBag();
+        TestRunner.testResizableArrayBag();
+        TestRunner.testLinkedBag();
+
+        BagInterface<String> bag1 = new ResizableArrayBag<>();
+        BagInterface<String> bag2 = new ResizableArrayBag<>();
+    }
+    private String add1(String bag1){
+
+
+
+        return bag1;
+    }
+    private String add2(String bag2){
+
+
+        return bag2;
     }
 }
