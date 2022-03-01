9a9c1ca72f1c3e388491ed5edb1e9e44eec2617e
Koki <kyamaguchi@cpp.edu>
2022-02-28 20:12:33 -0800

    added the private add method into the driver class

--- src/BagDriver.java
+++ src/BagDriver.java
@@ -1,21 +1,34 @@
-
+import java.util.Arrays;
 public class BagDriver{
+
+
     public static void main(String[] args){
+
         TestRunner.testResizableArrayBag();
         TestRunner.testLinkedBag();
 
-        BagInterface<String> bag1 = new ResizableArrayBag<>();
-        BagInterface<String> bag2 = new ResizableArrayBag<>();
+        BagInterface<String> aBag = new ResizableArrayBag<>();
+
+        char[] Bag1 = {'a', 'a', 'b', 'b', 'b', 'c', 'd', 'e'};
+        add(aBag, Bag1);
+
+        char[] Bag2 = {'a', 'b', 'b', 'e', 'e'};
+        add(aBag, Bag2);
+
+
+        System.out.println("Union in two bag: " + Arrays.toString(Bag1.union(Bag2).toArray()));
+
+        System.out.println("Intersection in two bag: " + Arrays.toString(Bag1.intersection(Bag2).toArray()));
+
+        System.out.println("Difference in two bags: " + Arrays.toString(Bag1.difference(Bag2).toArray()));
     }
-    private String add1(String bag1){
-
-
-
-        return bag1;
+    private static void add(BagInterface<String> aBag, char[] content){
+        for(int i = 0; i < content.length; i++){
+            aBag.add(String.valueOf(content[i]));
+            System.out.println(content[i] + " ");
     }
-    private String add2(String bag2){
-
-
-        return bag2;
+        System.out.println();
     }
+
+
 }
