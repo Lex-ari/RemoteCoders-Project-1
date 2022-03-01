08b71bf1808f2b14d78ce2647602023eede5a246
Alex Mariano <amarianocello@gmail.com>
2022-02-28 22:22:08 -0800

    Fixed BagDriver.java methods and demo code works now - Alex

--- src/BagDriver.java
+++ src/BagDriver.java
@@ -7,38 +7,42 @@ public class BagDriver{
         TestRunner.testResizableArrayBag();
         TestRunner.testLinkedBag();
 
-        BagInterface<String> Bag1 = new ResizableArrayBag<>();
-        BagInterface<String> Bag2 = new ResizableArrayBag<>();
+        System.out.println("\n == Start of Demo Program== \n");
+        BagInterface demoBag1 = new ResizableArrayBag<>();
+        BagInterface demoBag2 = new ResizableArrayBag<>();
 
         char[] contentsOfBag1 = {'a', 'a', 'b', 'b', 'b', 'c', 'd', 'e'};
-        addDemo(Bag1, contentsOfBag1);
+        addDemo(demoBag1, contentsOfBag1);
 
         char[] contentsOfBag2 = {'a', 'b', 'b', 'e', 'e'};
-        addDemo(Bag2, contentsOfBag2);
+        addDemo(demoBag2, contentsOfBag2);
 
 
-        System.out.println(Bag.unionDemo());
+        unionDemo(demoBag1, demoBag2);
 
-        System.out.println(aBag.intersectionDemo());
+        intersectionDemo(demoBag1, demoBag2);
 
-        System.out.println(aBag.differenceDemo());
+        System.out.println("\nDemo Difference using demoBag1.difference(demoBag2)");
+        differenceDemo(demoBag1, demoBag2);
+        System.out.println("Demo Difference using demoBag2.difference(demoBag1)");
+        differenceDemo(demoBag2, demoBag1);
     }
-    private static void addDemo(BagInterface<String> aBag, char[] content){
+    private static void addDemo(BagInterface<Character> aBag, char[] content){
+        System.out.println("Adding to new bag");
         for(int i = 0; i < content.length; i++){
-            aBag.add(String.valueOf(content[i]));
-            System.out.println(content[i] + " ");
+            aBag.add(content[i]);
+            System.out.print(content[i] + " ");
     }
         System.out.println();
     }
-    private static void unionDemo(BagInterface<String> aBag){
+    private static void unionDemo(BagInterface aBagEntry, BagInterface bBagEntry){
 
-        BagInterface<String> everything = bag1.union(bag2);
-        System.out.println("Union in two bag: " + everything);
+        System.out.println("Union in two bags: " + Arrays.toString(aBagEntry.union(bBagEntry).toArray()));
     }
-    private static void intersectionDemo(){
-        System.out.println("Intersection in two bag: " + Arrays.toString(aBag.intersection().toArray()));
+    private static void intersectionDemo(BagInterface<String> aBagEntry, BagInterface<String> bBagEntry){
+        System.out.println("Intersection in two bags: " + Arrays.toString(aBagEntry.intersection(bBagEntry).toArray()));
     }
-    private static void differenceDemo(){
-        System.out.println("Difference in two bags: " + Arrays.toString(Bag1.difference(Bag2).toArray()));
+    private static void differenceDemo(BagInterface<String> aBagEntry, BagInterface<String> bBagEntry){
+        System.out.println("Difference in two bags: " + Arrays.toString(aBagEntry.difference(bBagEntry).toArray()));
     }
 }
